3a54c67bea4ba834220f9947e1ba3937b36244c9
Jess Goodlett <jessica.goodlett@passare.com>
2023-04-10 10:17:09 -0500

    Added router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Home = {template: ''}
+
+const routes = [
+  {path: '/', component: Home},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(app);
+app.use(router)
+app.mount('#app')
+
+
+// createApp(App).mount('#app')
