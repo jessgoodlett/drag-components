60cfa0fd9ecc4a15f16a2f76f094c5a1a8a296c9
Jess Goodlett <jessgoodlett@gmail.com>
2023-05-21 16:58:58 -0500

    Simple content changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>Draggable Components</h1>
-  <div>
+  <div class="links">
     <a href="/drag">Drag in Window</a>
+    <a href="/drag-n-drop">Drag and Drop</a>
   </div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped>
+<style>
+.links {
+  display: flex;
+  flex-direction: column;
+}
 </style>
---
+++ src/views/SimpleDrag.vue
@@ -0,0 +1,7 @@
+<script setup>
+import DraggableContainer from '../components/DraggableContainer.vue'
+</script>
+
+<template>
+  <DraggableContainer />
+</template>
